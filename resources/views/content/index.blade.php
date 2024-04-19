6a5eee90cd5fabefd73cae5fe20cc071a547eca3
[Bennart <[bennart.dgunawan@gmail.com]>
2024-04-19 16:28:56 +0700

    0.1 Routing

---
+++ resources/views/content/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.layout')
+
+@section('content')
+  <div>
+    APPLY
+  </div>
+@endsection
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="asset/logo/A.png" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <title>APPLY</title>
+</head>
+
+<body>
+
+  @yield('content')
+
+</body>
+
+</html>
