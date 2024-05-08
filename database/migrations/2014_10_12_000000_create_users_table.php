26d22cb27d2ea293f5957bbb2d5bdf7de957fd78
[Addien <[Addiewmuhammad25@gmail.com]>
2024-05-08 16:54:45 +0700

    0.2 [migrate:users]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('user_name');
+            $table->string('user_email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('user_password');
+            $table->string('user_username')->unique();
+            $table->string('user_city');
+            $table->string('user_country');
+            $table->string('user_status');
             $table->rememberToken();
             $table->timestamps();
         });
